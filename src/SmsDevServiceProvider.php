e5dc1e09e1ff0f64bb99cabb09d504ea25b4626e
Lucas Giovanny <lucasgiovanny@gmail.com>
2020-05-15 22:52:29 -0300

    Create service provider

---
+++ src/SmsDevServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace lucasgiovanny\SmsDev\Tests;
+
+use GuzzleHttp\Client as HttpClient;
+use Illuminate\Support\ServiceProvider;
+use lucasgiovanny\SmsDev\SmsDev;
+use lucasgiovanny\SmsDev\SmsDevChannel;
+
+class SmsDevServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->app->when(SmsDevChannel::class)->needs(SmsDev::class)->give(function () {
+            $apiKey = config('services.smsdev.api_key');
+            return new SmsDev($apiKey, new HttpClient());
+        });
+    }
+
+    public function register()
+    {
+    }
+}
